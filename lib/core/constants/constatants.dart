95ea620df80a19df9dfecd2bafd2b253af019fb1
Abhinav P <abhinavp9074@gmail.com>
2024-03-17 11:08:00 +0530

    project finished

---
+++ lib/core/constants/constatants.dart
@@ -0,0 +1,6 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'e1df1cd2628c4c8b843259e7a302e0cb';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
+const String kDefaultImage =
+    "https://png.pngtree.com/png-clipart/20210129/ourmid/pngtree-default-male-avatar-png-image_2811083.jpg";
